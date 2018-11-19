229f897ad8db79212a90bed05bac247bee77ec33
MohsinRazzaq <mohsinrazzaq25@gmail.com>
2018-11-19 16:41:13 +0500

    added contact form

---
+++ js/script.js
@@ -0,0 +1,33 @@
+jQuery(function ($) {
+
+    'use strict';
+
+    (function () {
+        $('#successmail').hide();
+        $('#errormail').hide();
+        $('#preloader').delay(200).fadeOut('slow');
+    }());
+
+    $(function () {    
+        $("#contactForm").submit(function (e) {
+            e.preventDefault();
+            $('#successmail').hide(500);
+            $('#errormail').hide(500);
+            var form_data = $(this).serialize(); 
+            $.ajax({
+                type: "POST", 
+                url: "mail.php",
+                dataType: "json", // Add datatype
+                data: form_data
+            }).done(function (data) {
+                console.log(data);
+                $('#errormail').hide();                
+                $('#successmail').show(700);                
+                $('#contactForm').trigger("reset");
+            }).fail(function (data) {
+                $('#successmail').hide();                
+                $('#errormail').show(700);                                
+                console.log(data);            
+            });
+        }); 
+    });
